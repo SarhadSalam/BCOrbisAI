7055c680e350966f6e762eaa5c98c2db9e756ecd
William Boyle <spitford@Williams-MacBook-Pro.local>
2017-09-23 18:48:43 -0400

    So far....

--- LUMINIS/Bots/BCBot/PlayerAI.py
+++ LUMINIS/Bots/BCBot/PlayerAI.py
@@ -2,6 +2,7 @@ from PythonClientAPI.Game import PointUtils
 from PythonClientAPI.Game.Entities import FriendlyUnit, EnemyUnit, Tile
 from PythonClientAPI.Game.Enums import Direction, MoveType, MoveResult
 from PythonClientAPI.Game.World import World
+import time
 
 class PlayerAI:
 
@@ -10,7 +11,9 @@ class PlayerAI:
         Any instantiation code goes here
         """
         self.count = 0
+        self.defend = [0]
         self.movespot=[0,0]
+        self.defendlist = [[0,-1],[0,1],[1,0],[-1,0]]
         self.movelist = [[-1,0],[-1,0],[0,1],[-1,0],[-1,0],[0,-1],[0,-1],[1,0]]
         pass
 
@@ -28,14 +31,20 @@ class PlayerAI:
         # Take over the world
 
         ourNests = world.get_friendly_nest_positions()
-
-        for unit in friendly_units:
-            for nest in ourNests:
+        if (len(self.defend) != len(ourNests)):
+            self.defend += [0]
+        for i in range(len(ourNests)):
+            nest = ourNests[i]
+            for unit in friendly_units:
                 if unit == friendly_units[0]:
                     self.movespot = [unit.position[0] + self.movelist[self.count][0], unit.position[1] + self.movelist[self.count][1]]
                     self.count += 1
-                    if self.count == 7:
+                    if self.count == len(self.movelist):
                         self.count = 0
                 elif (unit.position == nest):
-                    self.movespot = [nest[0], nest[1] - 1]
+                    self.movespot = [nest[0] + self.defendlist[self.defend[i]][0], nest[1] + self.defendlist[self.defend[i]][1]]
+                    self.defend[i] += 1
+                    if self.defend[i] == len(self.defendlist):
+                        self.defend[i] = 0
                 world.move(unit, self.movespot)
+        time.sleep(0.5)
